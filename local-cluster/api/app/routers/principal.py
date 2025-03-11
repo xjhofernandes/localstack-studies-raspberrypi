21772a0705326393ff0c6de5445d17a02794b619
Jonathan Fernandes <xjhofernandes@gmail.com>
2025-03-11 04:59:58 -0300

    :bulb: - Adding deployment deployment and service to the Local API

--- local-cluster/api/app/routers/principal.py
+++ local-cluster/api/app/routers/principal.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import APIRouter
 from app.service import principal
 
@@ -10,3 +11,13 @@ router = APIRouter(
 async def hello_world(name: str = None) -> str:
     return principal.hello_world(name)
 
+
+@router.get("/health")
+async def health_check():
+    return {"status": "ok"}
+
+
+@router.get("/getpod")
+async def hello():
+    pod_name = os.getenv("HOSTNAME", "unknown-pod")  # Get pod name from environment
+    return {"message": "Hello, World!", "pod": pod_name}
